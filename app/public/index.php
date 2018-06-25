ebbd1415386dafa81a6949df8bef49bdc9d13237
ohtacaesar <ohtacaesar@gmail.com>
2018-06-26 08:08:26 +0900

    SESSIONをRedisで管理する

--- app/public/index.php
+++ app/public/index.php
@@ -2,14 +2,24 @@
 
 $sessionStartOptions = [];
 
-foreach(['cookie_domain', 'cookie_secure'] as $key) {
+foreach (['cookie_domain', 'cookie_secure', 'save_handler', 'save_path'] as $key) {
     $KEY = 'SESSION_' . strtoupper($key);
     if (isset($_ENV[$KEY])) {
         $sessionStartOptions[$key] = $_ENV[$KEY];
     }
 }
 
-$redis = new Redis();
-$redis->connect('redis', 6379);
-var_dump($redis->get('test'));
-$redis->close();
+session_start($sessionStartOptions);
+
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SESSION DUMP</title>
+</head>
+
+<body>
+<pre><?php var_dump($_SESSION) ?></pre>
+</body>
+
+</html>
