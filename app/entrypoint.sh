44e5c15baeefa06eb3acd21a8973d719c61a7d7b
ohtacaesar <ohtacaesar@gmail.com>
2018-06-27 21:20:26 +0900

    Sessionの設定をファイルに書き出す

---
+++ app/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+env | grep SESSION | sed 's/_/./' | tr [A-Z] [a-z] > /usr/local/etc/php/conf.d/session.ini
+
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- php-fpm "$@"
+fi
+
+exec "$@"
