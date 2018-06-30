5758330818c2bc5e441e463b2dcbc7dfb205a0bb
ohtacaesar <ohtacaesar@gmail.com>
2018-06-30 22:34:25 +0900

    yamlでDB接続情報を管理

--- php-auth/public/index.php
+++ php-auth/public/index.php
@@ -1,1 +1,15 @@
 <?php
+
+
+$conf = yaml_parse_file("config.yml");
+
+$pdo = new \PDO(
+    $conf['pdo']['dsn'],
+    $conf['pdo']['username'],
+    $conf['pdo']['passwd'],
+    [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
+
+var_dump($pdo->query("select 1 as a")->fetchAll());
