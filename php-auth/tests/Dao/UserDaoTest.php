69d548dcf15cc914ff978537c2618c0a6885e18d
ohtacaesar <ohtacaesar@gmail.com>
2018-07-15 13:49:16 +0900

    PHP Unitのインストール

---
+++ php-auth/tests/Dao/UserDaoTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dao;
+
+use PHPUnit\Framework\TestCase;
+
+class UserDaoTest extends TestCase
+{
+
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+
+}
