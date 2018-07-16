b4f92537ed978b5a029d567de46c3a324e06f4ad
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2018-07-17 08:46:44 +0900

    Change domains for google oauth

--- php-app/public/index.php
+++ php-app/public/index.php
@@ -14,16 +14,16 @@ session_start(['read_and_close' => 1]);
 <body>
 
 <p>
-    <a href="//auth.localhost.test:8080/github.php?from=http://<?php echo $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] ?>">ログイン</a>
+    <a href="//auth.localhost.dev/github.php?from=http://<?php echo $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] ?>">ログイン</a>
     |
-    <a href="//auth.localhost.test:8080/logout.php?from=http://<?php echo $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] ?>">ログアウト</a>
+    <a href="//auth.localhost.dev/logout.php?from=http://<?php echo $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] ?>">ログアウト</a>
     <?php if (isset($_SESSION['login'])): ?>| ログイン済み<?php endif; ?>
 </p>
 
 <ul>
     <li><a href="phpinfo.php">phpinfo</a></li>
     <li><a href="postgresql.php">postgresql</a></li>
-    <li><a href="http://auth.localhost.test:8080/users.php">users</a></li>
+    <li><a href="http://auth.localhost.dev/users.php">users</a></li>
 </ul>
 
 <pre><?php var_dump($config) ?></pre>
