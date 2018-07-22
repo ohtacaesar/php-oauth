67939a067b76a77a068d9daeafbb812f19326e18
ohtacaesar <ohtacaesar@gmail.com>
2018-07-22 23:47:25 +0900

    ProvidersをENUMっぽく

---
+++ php-auth/tests/Unit/Util/ProvidersTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Util;
+
+use PHPUnit\Framework\TestCase;
+use Util\ProviderException;
+use Util\Providers;
+
+class ProvidersTest extends TestCase
+{
+    public function testValues()
+    {
+        $values = Providers::values();
+        $this->assertArrayHasKey('GITHUB', $values);
+        $this->assertArrayHasKey('GOOGLE', $values);
+    }
+
+    public function testValueOf()
+    {
+        $this->assertEquals(1, Providers::valueOf('GITHUB'));
+        $this->assertEquals(1, Providers::valueOf('GitHub'));
+        $this->assertEquals(2, Providers::valueOf('GOOGLE'));
+        $this->assertEquals(2, Providers::valueOf('Google'));
+        try {
+            Providers::valueOf('GIFHUB');
+            $this->fail();
+        } catch (\LogicException $e) {
+        }
+        try {
+            Providers::valueOf('GIFHUB');
+            $this->fail();
+        } catch (\LogicException $e) {
+        }
+    }
+}
