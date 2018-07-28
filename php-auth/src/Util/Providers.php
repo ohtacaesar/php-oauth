3043eb0497f9990dc5b4473f0edd3d0f9f0aba36
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2018-07-28 09:56:04 +0900

    Extract Enum class

--- php-auth/src/Util/Providers.php
+++ php-auth/src/Util/Providers.php
@@ -2,36 +2,8 @@
 
 namespace Util;
 
-class Providers
+class Providers extends Enum
 {
-    private static $values = null;
-
     const GITHUB = 1;
     const GOOGLE = 2;
-
-    private static function init(): void
-    {
-        if (self::$values !== null) {
-            return;
-        }
-        $class = new \ReflectionClass(self::class);
-        self::$values = $class->getConstants();
-    }
-
-    public static function values(): array
-    {
-        self::init();
-        return self::$values;
-    }
-
-    public static function valueOf(string $name): int
-    {
-        self::init();
-        $name = strtoupper($name);
-        if (!isset(self::$values[$name])) {
-            throw new \LogicException(sprintf("Providers::valueOf('%s')", $name));
-        }
-
-        return self::$values[$name];
-    }
 }
