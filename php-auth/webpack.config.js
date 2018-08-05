96dc3034d1f42d6427c8a24dfb146266e62cc44c
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2018-08-05 13:36:53 +0900

    ExtractTextPluginを使ってcssをbundle.jsから分離

--- php-auth/webpack.config.js
+++ php-auth/webpack.config.js
@@ -1,16 +1,21 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-    mode: process.env.NODE_ENV || "development",
-    entry: ["./src/js/index.js"],
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/public/dist"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
+  mode: process.env.NODE_ENV || "development",
+  entry: ["./src/js/index.js"],
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/public/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({use: 'css-loader'})
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
