faef822b5e5745ecb1f404f0206e987e9a5cd33d
ohtacaesar <ohtacaesar@gmail.com>
2018-08-22 22:29:30 +0900

    X-AUTH-ENABLE

--- tests/Functional/AuthTest.php
+++ tests/Functional/AuthTest.php
@@ -8,25 +8,41 @@ class AuthTest extends BaseTestCase
 
     public function test1()
     {
-        $r = $this->runApp('GET', '/auth');
+        $r = $this->runApp('GET', '/auth', null, [], [
+            'HTTP_X_AUTH_ENABLE' => 1,
+        ]);
         $this->assertEquals(401, $r->getStatusCode());
     }
 
     public function test2()
     {
-        $r = $this->runApp('GET', '/auth', null, ['user_id' => 'user']);
+        $r = $this->runApp('GET', '/auth', null, ['user_id' => 'user'], [
+            'HTTP_X_AUTH_ENABLE' => 1,
+        ]);
         $this->assertEquals(200, $r->getStatusCode());
     }
 
     public function test3()
     {
-        $r = $this->runApp('GET', '/auth', null, ['user_id' => 'user'], ['HTTP_X_AUTH_ROLES' => 'ADMIN']);
+        $r = $this->runApp('GET', '/auth', null, ['user_id' => 'user'], [
+            'HTTP_X_AUTH_ENABLE' => 1,
+            'HTTP_X_AUTH_ROLES' => 'ADMIN',
+        ]);
         $this->assertEquals(403, $r->getStatusCode());
     }
 
     public function test4()
     {
-        $r = $this->runApp('GET', '/auth', null, ['user_id' => 'admin'], ['HTTP_X_AUTH_ROLES' => 'ADMIN']);
+        $r = $this->runApp('GET', '/auth', null, ['user_id' => 'admin'], [
+            'HTTP_X_AUTH_ENABLE' => 1,
+            'HTTP_X_AUTH_ROLES' => 'ADMIN',
+        ]);
         $this->assertEquals(200, $r->getStatusCode());
+    }
+
+    public function test5()
+    {
+        $r = $this->runApp('GET', '/auth', null, ['user_id' => 'admin']);
+        $this->assertEquals(400, $r->getStatusCode());
     }
 }
